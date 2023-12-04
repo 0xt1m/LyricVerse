79104ac48b6309494316138653270124bab18efc
Tymophiyy <tima.matviiv@gmail.com>
2023-12-04 08:43:21 -0500

    rm .DS_Store

--- main.py
+++ main.py
@@ -34,8 +34,11 @@ def run_updater():
 	os.system("python3 updater.py")
 
 c_version = open("version.txt", "r").read()
-n_version = requests.get("http://localhost:8000/new_version").json()['latest_version']
-if c_version != n_version:
+try:
+	n_version = requests.get("http://localhost:8000/new_version").json()['latest_version']
+except:
+	n_version = ""
+if n_version and c_version != n_version:
 	t = threading.Thread(target=run_updater)
 	t.start()
 	quit()
